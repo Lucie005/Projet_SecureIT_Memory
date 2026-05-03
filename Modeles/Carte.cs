b22e17b1fcb2230c214e1d1d61a7868d5fcd6cd0
Thibaud et tabard <thibaudtabard@gmail.com>
2026-05-03 22:37:54 +0200

    Implémentation de la logique métier, encapsulation de la classe Carte et gestion du Memory

---
+++ Modeles/Carte.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SecurIT_Memory.Modeles 
+{
+    public enum EtatCarte
+    {
+        Cachee,
+        Revelee,
+        Trouvee
+    }
+
+    public class Carte
+    {
+        private int _id;
+        private string _cheminImage;
+        private EtatCarte _etat;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string CheminImage
+        {
+            get { return _cheminImage; }
+            set { _cheminImage = value; }
+        }
+
+        public EtatCarte Etat
+        {
+            get { return _etat; }
+            set { _etat = value; }
+        }
+
+        public Carte(int id, string cheminImage)
+        {
+            _id = id;
+            _cheminImage = cheminImage; 
+            _etat = EtatCarte.Cachee; 
+        }
+    }
+}
---
+++ Modeles/JeuMemory.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SecurIT_Memory.Modeles
+{
+    public class JeuMemory
+    {
+        public List<Carte> Cartes { get; private set; }
+        public int NbEssais { get; private set; } // Ajout du compteur d'essais
+
+        public JeuMemory()
+        {
+            Cartes = new List<Carte>();
+            NbEssais = 0;
+        }
+
+        public void InitialiserJeu(List<string> cheminsImages)
+        {
+            Cartes.Clear();
+            NbEssais = 0; // Réinitialisation des essais
+            int id = 0;
+
+            foreach (string chemin in cheminsImages)
+            {
+                Cartes.Add(new Carte(id, chemin));
+                Cartes.Add(new Carte(id, chemin));
+                id++;
+            }
+
+            MelangerCartes();
+        }
+
+        private void MelangerCartes()
+        {
+            Random rng = new Random();
+            int n = Cartes.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                Carte value = Cartes[k];
+                Cartes[k] = Cartes[n];
+                Cartes[n] = value;
+            }
+        }
+
+        // --- LOGIQUE DE JEU ---
+
+        // Évalue si les deux cartes sont identiques
+        public bool EvaluerPaire(Carte c1, Carte c2)
+        {
+            NbEssais++;
+
+            if (c1.CheminImage == c2.CheminImage)
+            {
+                c1.Etat = EtatCarte.Trouvee;
+                c2.Etat = EtatCarte.Trouvee;
+                return true;
+            }
+            return false;
+        }
+
+        // Vérifie si toutes les cartes sont trouvées
+        public bool VerifierVictoire()
+        {
+            return Cartes.TrueForAll(c => c.Etat == EtatCarte.Trouvee);
+        }
+    }
+}
